805c3a37e2a2f7dc2ee61503d4268d5ca8c91f45
MostofaAsik <mmostofaasik@gmail.com>
2024-12-11 14:26:52 +0600

    Signing method implementation

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,4 +1,4 @@
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth';
 import React, { createContext, useState } from 'react';
 import { auth } from '../firebase/firebase.config';
 
@@ -8,16 +8,47 @@ const AuthProvider = ({ children }) => {
     const [user, setUser] = useState('')
     const [loading, setLoading] = useState(true)
 
+    const googleProvider = new GoogleAuthProvider()
+
     const createUser = (email, password) => {
         setLoading(true)
         return createUserWithEmailAndPassword(auth, email, password)
 
     }
+    const createSignIn = (email, password) => {
+        setLoading(true)
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    const signOutUser = () => {
+        setLoading(true)
+        return signOut(auth)
+
+    }
+
+    const goooleLogin = () => {
+        setLoading(true)
+        return signInWithPopup(auth, googleProvider)
+    }
+    useEffect(() => {
+        const unSubscribe = onAuthStateChanged(auth, (curentUser) => {
+            console.log('current user from authProvider', curentUser);
+            setUser(curentUser)
+            setLoading(false)
+
+        });
+        return () => {
+            unSubscribe();
+        }
+    }, [])
 
     const authInfo = {
         user,
         loading,
-        createUser
+        createUser,
+        createSignIn,
+        signOutUser,
+        goooleLogin
     }
 
     return (
